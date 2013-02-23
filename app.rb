83d2a06f5068317e0aedc5fe9879068ede0c7552
owain lewis <owain@owainlewis.com>
2013-02-23 22:47:51 +0000

    heroku ready

--- app.rb
+++ app.rb
@@ -5,16 +5,15 @@
 # ====================================
 
 require 'sinatra'
-require 'haml'
 
 helpers do
   def partial(page, options={})
-    haml page.to_sym, options.merge!(:layout => false)
+    erb page.to_sym, options.merge!(:layout => false)
   end
 end
 
 # Page routes
 
 get '/' do
-  haml :index
+  erb :index
 end
